2f6f510aa5570ebc3cbcdeadc9c9aedfbacce857
ayildirimnec <87165930+ayildirimnec@users.noreply.github.com>
2021-07-30 13:42:17 +1000

    Update network.tf

--- network.tf
+++ network.tf
@@ -20,7 +20,7 @@ resource "azurerm_network_ddos_protection_plan" "example" {
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "virtualNetwork1"
+  name                = "virtualNetwork3"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
